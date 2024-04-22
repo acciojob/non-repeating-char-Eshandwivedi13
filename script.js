f9340821038ab2c5852f1eec62b5c736ef0cd2ab
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-22 18:42:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function firstNonRepeatedChar(str) {
-  if(str === "") return null;
- let newStr = str.split("").sort().join("");
-	let char = str[0];
-   for(let ch of str){
-	   if(ch != char) return ch;
-		char = ch;
-   }
-	return null;
-}
-const input = prompt("Enter a string");
+  let counts = {};//object is working as freqArr or HashSet
+    for(let i = 0; i < str.length; i++){
+        let char = str[i];
+        counts[char] = counts[char] ? counts[char] + 1 : 1;//if already exists, incr value..otherWise insert 1
+    }
+    for(let i = 0; i < str.length; i++){
+        if(counts[str[i]] === 1){//whichever character in obj has value 1, return it
+            return str[i];
+        }
+    }
+    return null;
+}  
+const input = prompt("Enter a string"); 
 alert(firstNonRepeatedChar(input)); 
