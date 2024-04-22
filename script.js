80c066d4cec6b645adf44190600d7bf7f7bdef37
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-22 18:33:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  if(str === "") return null;
+ let newStr = str.split("").sort().join("");
+	let char = str[0];
+   for(let ch of str){
+	   if(ch != char) return ch;
+		char = ch;
+   }
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
